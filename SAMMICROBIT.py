2eee05fd4a9cda0e97eeeedb68ad9fad360dfc49
unfinishedd <sam0000@live.nl>
2021-10-25 14:52:05 +0200

    sam microbit

---
+++ SAMMICROBIT.py
@@ -0,0 +1,118 @@
+from microbit import *
+import radio
+import random
+
+def pairing():
+    radio.on()
+    while True:
+        display.show(Image(
+            "00000:"
+            "00000:"
+            "00000:"
+            "00000:"
+            "90000"))
+        sleep(500)
+        display.show(Image(
+            "00000:"
+            "00000:"
+            "99000:"
+            "00900:"
+            "90900"))
+        sleep(500)
+        display.show(Image(
+            "99900:"
+            "00090:"
+            "99009:"
+            "00909:"
+            "90909"))
+        sleep(500)
+        pairing = radio.receive()
+        if pairing == 'working':
+            radio.send('tyfus')
+            paired()
+            break
+
+
+
+def paired():
+    display.clear()
+    display.show(Image(
+        "00000:"
+        "00000:"
+        "00000:"
+        "90000:"
+        "00000"))
+    sleep(500)
+    display.show(Image(
+        "00000:"
+        "00000:"
+        "00000:"
+        "90000:"
+        "09000"))
+    sleep(500)
+    display.show(Image(
+        "00000:"
+        "00000:"
+        "00000:"
+        "90900:"
+        "09000"))
+    sleep(500)
+    display.show(Image(
+        "00000:"
+        "00000:"
+        "00090:"
+        "90900:"
+        "09000"))
+    sleep(500)
+    display.show(Image(
+        "00000:"
+        "00009:"
+        "00090:"
+        "90900:"
+        "09000"))
+    sleep(10000)
+    recievernumber()
+
+def recievernumber():
+    radio.off()
+    radio.on()
+    display.clear()
+    sleep(20000)
+    number_receive = radio.receive();
+    display.scroll(str(number_receive))
+    sleep(10000)
+    time_in_seconds = 60 * int(number_receive)
+    display.scroll(time_in_seconds)
+    sleep(3000)
+
+
+
+
+
+def startup_screen():
+    display.show(Image(
+        "00000:"
+        "00900:"
+        "09990:"
+        "00900:"
+        "00000"))
+    sleep(500)
+    display.show(Image(
+        "00000:"
+        "09990:"
+        "09990:"
+        "09990:"
+        "00000"))
+    sleep(500)
+    display.show(Image(
+        "90909:"
+        "09990:"
+        "99999:"
+        "09990:"
+        "90909"))
+    sleep(1000)
+    pairing()
+    display.clear()
+
+
+startup_screen()

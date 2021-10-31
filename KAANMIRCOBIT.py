85219ed58a7a93dfc937358556b1df992522c869
Kaan Secen <24114@ma-web.nl>
2021-10-31 21:47:09 +0100

    finished product

--- KAANMIRCOBIT.py
+++ KAANMIRCOBIT.py
@@ -3,6 +3,7 @@ import radio
 import random
 
 def pairing():
+    radio.off()
     radio.on()
     while True:
         display.show(Image(
@@ -75,7 +76,7 @@ def paired():
 
 def sendnumber():
     display.clear()
-    random_number = random.randint(1,2)
+    random_number = random.randint(1, 2)
     display.show(Image(
         "09990:"
         "90909:"
@@ -86,9 +87,86 @@ def sendnumber():
     display.scroll(str(random_number) + "M")
     sleep(10000)
     radio.send(str(random_number))
+    timerrunout()
+    
 
-
-
+def timerrunout():
+    radio.off()
+    radio.on()
+    while True:
+        walking_time = radio.receive();
+        if walking_time == "walk":
+            walkingtime()
+            break
+        
+def walkingtime():
+    radio.off()
+    radio.on()
+    walking_time_steps = 2
+    display.scroll("walk")
+    sleep(5000)
+    while True:
+        walkingoff_time = radio.receive();
+        if walkingoff_time == "walkoff":
+            display.clear()
+            display.show(Image(
+                "00000:"
+                "00000:"
+                "00000:"
+                "90000:"
+                "00000"))
+            sleep(500)
+            display.show(Image(
+                "00000:"
+                "00000:"
+                "00000:"
+                "90000:"
+                "09000"))
+            sleep(500)
+            display.show(Image(
+                "00000:"
+                "00000:"
+                "00000:"
+                "90900:"
+                "09000"))
+            sleep(500)
+            display.show(Image(
+                "00000:"
+                "00000:"
+                "00090:"
+                "90900:"
+                "09000"))
+            sleep(500)
+            display.show(Image(
+                "00000:"
+                "00009:"
+                "00090:"
+                "90900:"
+                "09000"))
+            sleep(5000)
+            display.show(Image(
+                "00000:"
+                "00900:"
+                "09990:"
+                "00900:"
+                "00000"))
+            sleep(500)
+            display.show(Image(
+                "00000:"
+                "09990:"
+                "09990:"
+                "09990:"
+                "00000"))
+            sleep(500)
+            display.show(Image(
+                "90909:"
+                "09990:"
+                "99999:"
+                "09990:"
+                "90909"))
+            sleep(5000)
+            display.clear()
+            break
 
 
 def startup_screen():
--- SAMMICROBIT.py
+++ SAMMICROBIT.py
@@ -4,6 +4,7 @@ import random
 
 
 def pairing():
+    radio.off()
     radio.on()
     while True:
         display.show(Image(
@@ -90,12 +91,85 @@ def recievernumber():
     sleep(2000)
     while True:
         if time_in_seconds == 0:
-            display.scroll("W")
-            sleep(10000)
+            radio.send('walk')
+            walkingtime()
+            break
         display.scroll(time_in_seconds)
         sleep(1000)
         time_in_seconds -= 1
-        
+    
+    
+    
+def walkingtime():
+    walking_time_steps = 2
+    display.scroll("walk")
+    sleep(5000)
+    while True:
+        if accelerometer.was_gesture('shake'):
+            walking_time_steps -= 1
+            display.scroll(str(walking_time_steps))
+            sleep(2000)
+        if walking_time_steps == 0:
+            radio.send('walkoff')
+            display.clear()
+            display.show(Image(
+                "00000:"
+                "00000:"
+                "00000:"
+                "90000:"
+                "00000"))
+            sleep(500)
+            display.show(Image(
+                "00000:"
+                "00000:"
+                "00000:"
+                "90000:"
+                "09000"))
+            sleep(500)
+            display.show(Image(
+                "00000:"
+                "00000:"
+                "00000:"
+                "90900:"
+                "09000"))
+            sleep(500)
+            display.show(Image(
+                "00000:"
+                "00000:"
+                "00090:"
+                "90900:"
+                "09000"))
+            sleep(500)
+            display.show(Image(
+                "00000:"
+                "00009:"
+                "00090:"
+                "90900:"
+                "09000"))
+            sleep(5000)
+            display.show(Image(
+                "00000:"
+                "00900:"
+                "09990:"
+                "00900:"
+                "00000"))
+            sleep(500)
+            display.show(Image(
+                "00000:"
+                "09990:"
+                "09990:"
+                "09990:"
+                "00000"))
+            sleep(500)
+            display.show(Image(
+                "90909:"
+                "09990:"
+                "99999:"
+                "09990:"
+                "90909"))
+            sleep(5000)
+            display.clear()
+            break
 
 
 def startup_screen():
